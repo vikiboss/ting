c5fdd427dd373574b3cb5fcf45c009cd76995d6a
Viki <vikiboss@qq.com>
2020-12-29 10:34:45 +0800

    :zap: 差不多得了

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Lottie from 'react-lottie';
-import duration from 'dayjs/plugin/duration';
 import dayjs from 'dayjs';
 import 'normalize.css';
 
@@ -9,12 +8,21 @@ import heartData from './assets/heart.json';
 
 import './style.css';
 
-dayjs.extend(duration);
-
 const musicUrl = 'http://music.163.com/song/media/outer/url?id=17746129.mp3';
 const rainUrl = 'http://media.xxoojoke.com/tingyu/therain.m4a';
+
 const music = new Audio(musicUrl);
 const rain = new Audio(rainUrl);
+
+const play = () => {
+  music.play();
+  rain.play();
+};
+
+const pause = () => {
+  music.pause();
+  rain.pause();
+};
 
 const options = {
   loop: true,
@@ -24,35 +32,19 @@ const options = {
 
 const App: React.FC = () => {
   const size = useWinSize();
-  const [playing, setPlaying] = useState<boolean>(true);
   const [time, setTime] = useState<Date>(new Date());
-  const [diff, setDiff] = useState<number>(0);
-  const [timer, setTimer] = useState<any>(null);
+  const [playing, setPlaying] = useState<boolean>(true);
 
-  setInterval(() => setTime(new Date()), 1000);
-
-  const togglePlaying = () => {
-    if (playing) {
-      setTimer(setInterval(() => setDiff((diff) => diff + 1000), 1000));
-    } else {
-      clearInterval(timer);
-      setDiff(0);
-    }
-    setPlaying(!playing);
-  };
+  const togglePlaying = () => setPlaying(!playing);
 
   useEffect(() => {
-    if (playing) {
-      music.pause();
-      rain.pause();
-    } else {
-      music.play();
-      rain.play();
-    }
-  }, [playing]);
+    setInterval(() => setTime(new Date()), 1000);
+  }, []);
 
-  const timerText = dayjs(diff).subtract(8, 'h').format('HH:mm:ss');
-  const now = dayjs(time).format('HH:mm:ss ddd. MMM.D');
+  useEffect(() => (playing ? pause() : play()), [playing]);
+
+  const now = dayjs(time).format('HH:mm:ss ddd. M/D/YYYY');
+  const playClass = playing ? '' : 'playing';
 
   const lottieProps = {
     width: '4rem',
@@ -61,24 +53,20 @@ const App: React.FC = () => {
     isPaused: playing,
   };
 
+  const wraperProps = {
+    style: { ...size },
+    onClick: togglePlaying,
+    className: `${playClass} bg flex`,
+  };
+
   return (
-    <div
-      style={{ ...size }}
-      onClick={togglePlaying}
-      className={`${!playing ? 'playing' : ''} bg`}
-    >
-      <div className='title'>
+    <div {...wraperProps}>
+      <div className='title flex'>
         <span>听雨</span>
         <Lottie {...lottieProps} style={{ margin: 0 }} />
       </div>
-      <div className={`${!playing ? 'playing-timer' : ''} timer`}>
-        {timerText}
-      </div>
-      <button
-        onClick={togglePlaying}
-        className={`${!playing ? 'playing-btn' : ''}`}
-      />
-      <div className={`${!playing ? 'playing' : ''} current-time`}>{now}</div>
+      <button onClick={togglePlaying} className={`${playClass} btn`} />
+      <div className={`${playClass} now`}>{now}</div>
     </div>
   );
 };
